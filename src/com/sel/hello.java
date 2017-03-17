f7071d7b2a7098393e42aa1431e5055e8a422aa7
lak <lak3108@yahoo.com>
2017-03-17 10:01:30 +0530

    first commit

---
+++ src/com/sel/hello.java
@@ -0,0 +1,15 @@
+package com.sel;
+
+public class hello {
+public static void main(String[] args)
+{
+int lak=1;
+double lkk = 10.01;
+String kal = "lak";
+System.out.println(lak);
+System.out.println(kal);
+System.out.println(lkk);
+	}
+	
+	
+}
