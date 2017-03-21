6e5cce8aa18cf8b00a57663c355d74530960f306
lak <lak3108@yahoo.com>
2017-03-21 22:05:05 +0530

    loops

---
+++ src/com/sel/Loops.java
@@ -0,0 +1,27 @@
+package com.sel;
+
+public class Loops {
+	public static void main(String [] args)
+	{
+      int i=2;
+		for (int j=2;j<=20;j=j+2)
+		{
+			System.out.println(j);
+			
+		}
+		while(i<=10)
+		
+			{
+				System.out.println(i);
+			    i=i*3;	
+			}
+		
+		
+		for (int k=1;k<=10;k++)
+		{
+			System.out.println(k*5);
+		}
+		
+	}
+
+}
